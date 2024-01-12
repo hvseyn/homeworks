b22af85543e4af2fff979ebb5e708522c2b77a95
hvseyn <h806595@gmail.com>
2024-01-12 15:23:01 +0400

    new folder added and files changed

---
+++ DAY21/main.js
@@ -0,0 +1,54 @@
+//!---TASK1---//
+// let user = "My name is Huseyn";
+// console.log(user);
+
+
+//!---TASK2---//
+// const name = "Huseyn";
+// console.log(name);
+
+
+//!---TASK3---//
+//person and $add
+
+
+//!---TASK4---//
+// let bookPrice = 750
+// let bookAmount = 14
+// let totalPrice = bookPrice * bookAmount
+// console.log(totalPrice);
+
+
+//!---TASK5---//
+// let kmhSpeed = 75;
+// let msSpeed = kmhSpeed * 10 / 36
+// console.log(msSpeed);
+
+
+//!---TASK6---//
+// const date = new Date();
+// let nowyear = date.getFullYear();
+// let year = prompt("Tezol tarixini de!");
+// let age = nowyear - year;
+// console.log("Bugune qeder " + age*365 + "gun yasamisan");
+
+
+//!---TASK7---//
+// let num = 13
+// console.log(num**5);
+// console.log(num%3);
+
+
+//!---TASK8---//
+// let firstName = "Huseyn";
+// let lastName = "Rahmanov";
+// console.log("Sizin adiniz " + firstName + " "+ lastName + "." + " Sad oldum!" );
+
+
+//!---TASK9---//
+// let firstName = "Huseyn";
+// let lastName = "Rahmanov";
+// console.log("Sizi bir daha gormeyimize sadiq, " + firstName + " " + lastName );
+
+
+//!---TASK10---//
