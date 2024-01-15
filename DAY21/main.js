6f9f65a000e0f7a0893ec39d462b223583440a61
hvseyn <h806595@gmail.com>
2024-01-15 17:56:20 +0400

    file inside changed

--- DAY21/main.js
+++ DAY21/main.js
@@ -52,3 +52,122 @@
 
 
 //!---TASK10---//
+// let name = "Huseyn Rahmanov"
+// console.log(name.length-1);
+
+
+//!---TASK11---//
+// let name = "Huseyn Rahmanov"
+// let spaceIndex = name.indexOf (" ")
+// console.log(name[0], name[spaceIndex +1]);
+
+
+//!---TASK12---//
+// let fullname = prompt("Adini daxil et");
+// let spaceIndex = fullname.indexOf (" ")
+// let name = fullname.slice (0, spaceIndex)
+// let surname = fullname.slice (spaceIndex +1)
+// console.log(surname, name);
+
+
+//!---TASK13---//
+// let name = prompt("Adini daxil et");
+// console.log("Salam, "+ name);
+
+
+//!---TASK14---//
+// let a = Number(prompt("Reqem daxil edin"));
+// let b = Number(prompt("Bir reqem daha daxil edin"));
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+
+//!---TASK15---//
+// let a = 12345
+// let b = 98765
+// let result = String(a*b)
+// console.log(result);
+// console.log(result[5]);
+
+
+//!---TASK16---//
+// console.log("" + 1 + 0);
+// console.log("" - 1 + 0);
+// console.log("true" - false);
+// console.log(6 / "3");
+// console.log(4 + 5 + "px");
+// console.log("$" + 4 +5);
+// console.log("4" - 2);
+// console.log("4px" - 2 + "px");
+// console.log(7/0);
+// console.log("-9" + 5);
+// console.log("-9" - 5);
+// console.log(null + 1);
+// console.log(undefined + 1);
+
+
+//!---TASK17---//
+// let num = prompt("4 reqemli eded daxil edin");
+// console.log(Number(num[0]) + Number(num[1]) + Number(num[2]) + Number(num[3]) );
+
+
+//!---TASK18---//
+// let fullname = prompt("Tam adini daxil et");
+// console.log("Salam, "+ fullname);
+
+
+//!---TASK19---//
+// let price = "530.90"
+// let pointindex = price.indexOf (".")
+// console.log(price.slice(0, pointindex) +" manat " + price.slice(pointindex+1)+ " qepik");
+
+
+//!---TASK20---//
+// let salary = prompt("Vergisiz maasinizi hesablamaq ucun hal hazirda aldiginiz maasi daxil edin:");
+// console.log(salary - salary*13/100);
+
+
+//!---TASK21---//
+// let fullname = prompt("adinizi soyadinizi ve ata adini daxil edin")
+// let spaceindex = fullname.indexOf (" ")
+// let fathername = fullname[fullname.lastIndexOf(" ") +1]
+// console.log(fullname.slice (0, spaceindex +2) + "." + fathername + "." );
+
+
+//!---TASK22---//
+// let salary = prompt("Depozit etmek istediyiniz meblegi daxil edin");
+// let month = prompt("Depozit etmek istediyiniz muddeti daxil edin(ay olaraq!)");
+// let percentage = prompt("Faiz derecesini daxil edin");
+// console.log("Sizin " + month +" ay ucun umumi geliriniz " + salary * percentage /100 + " manatdir." );
+
+
+//!---TASK23---//
+// let a = 20;
+// let b = 20;
+// console.log(a<b);
+// console.log(a!=b);
+// console.log(a<=b);
+// console.log(b>=a);
+
+
+//!---TASK24---//
+// let age = prompt("Yasinizi daxil edin!");
+// if (age < 18) {
+// console.log("Access denied!");
+// } else { 
+//     console.log("Access granted");
+// }
+
+
+//!---TASK25---//
+//!---TASK26---//
+
+
+let year = prompt ("Il daxil edin!");
+if (year % 4 == 0) {
+console.log("Uzun ildir");
+} else {
+    console.log("Uzun il deyil");
+}
