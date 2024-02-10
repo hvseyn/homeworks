c32cab5a098b132512cf6c4336c7dd63f2391344
hvseyn <h806595@gmail.com>
2024-02-11 01:52:52 +0400

    new files added

---
+++ DAY23/main.js
@@ -0,0 +1,11 @@
+// function islucky(val) {
+//     let strislucky = String(val)
+//     if (+val[0] + +val[1] + +val[2] === +val[3] + +val[4] + +val[5]) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+// console.log(islucky("124214"))
+
+
---
+++ DAY24/main.js
@@ -0,0 +1,14 @@
+let users = "Huseyn, Elmar, Ibrahim, Elmin";
+
+
+
+// let usersArr = ["Huseyn", "Elmar", "Ibrahim", "Elmin"];
+// console.log(usersArr[0]);
+// console.log(usersArr[1]);
+// console.log(usersArr[2]);
+// console.log(usersArr[3]);
+
+
+// for (let i =0; i <  usersArr.lenght; i++ ) {
+//     console.log(usersArr[1]);
+// }
