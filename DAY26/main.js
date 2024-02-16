8fb3890e7beedb45d6017c4a7947ae90db6df636
hvseyn <h806595@gmail.com>
2024-02-16 17:28:07 +0400

    file inside changed

--- DAY26/main.js
+++ DAY26/main.js
@@ -71,3 +71,72 @@
 //     }
 // }
 // positiveNumber(numbers);
+
+///!----TASK07----\\\
+// let names = ["Michael", "Trevor", "Franklin", "Lamar", "Jimmy"]
+
+// function printWithDashes(name) {
+//     console.log("-------------");
+//     console.log(name);
+//     console.log("-------------");
+// }
+
+// function printWithHearts(name) {
+//     console.log("<3<3<3<3 " + name + " <3<3<3<3");
+// }
+
+// function printWithIndex(name, index) {
+//     console.log(index + " - " + name);
+// }
+
+// names.forEach(printWithDashes);
+// names.forEach(printWithHearts);
+// names.forEach(printWithIndex);
+
+///!----TASK08----\\\
+// function kebabToUpper(text) {
+//     return text.toUpperCase().replace(/-/g, "_");
+// }
+// console.log(kebabToUpper("first-user"));
+
+///!----TASK09----\\\
+// let numbers = [1, 5, -7, 3, -9, 4, -6, 2];
+
+// let modifiedNum = numbers.map(function (numbers) {
+//     if (numbers < 0) {
+//         return -numbers;
+//     } else {
+//         return numbers * 2;
+//     }
+// })
+// console.log(modifiedNum);
+
+///!----TASK10----\\\
+// let strings = ["huseyn", "aztu", "realmadrid", "19"];
+// function getFirstThreeChars(arr) {
+
+
+//     let filtered = arr.filter(function (item) {
+//         return item.length >= 3;
+//     })
+
+//     if (filtered.length === 0) {
+//         return null;
+//     } else {
+//         return filtered[0].substring(0, 3);
+//     }
+// }
+// let result = getFirstThreeChars(strings);
+// console.log(result);
+
+///!----TASK11----\\\
+// let names = ["Aydan", "Huseyn", "Ayxan", "Ibrahim"];
+// let filteredNames = names.filter(function (name) {
+//     return name[0] === "A" || name[0] === "a"
+// })
+// console.log(filteredNames);
+
+///!----TASK12----\\\
+
+
+///!----TASK13----\\\
