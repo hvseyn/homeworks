7b20efca3a9eeb49cd9419ec5d199f665a5b4e18
hvseyn <h806595@gmail.com>
2024-03-01 11:26:53 +0400

    file inside changed

---
+++ DAY31/main.js
@@ -0,0 +1,40 @@
+// !----TASK01----//
+// const dom = document.querySelector("h1");
+// dom.remove()
+// dom.append("TITAN RTX")
+// console.log(dom);
+
+
+// !----TASK02----//
+// const li = document.createElement("li");
+// const liText = document.createTextNode("TITAN RTX")
+// li.appendChild(liText);
+// document.querySelector("ul").appendChild(li)
+
+
+// !----TASK03----//
+// const list = document.querySelectorAll("li");
+// list.forEach(li => {
+//     if (li.className === "primary") {
+//         li.style.background = "#ebebeb"
+//     }
+// })
+
+// !----TASK04----//
+// const primaryElements = document.querySelectorAll(".primary");
+// primaryElements.forEach(element => {
+//     console.log(element.textContent);
+// });
+
+
+// !----TASK05----//
+// const listItems = document.querySelectorAll('ul li');
+
+// listItems.forEach(item => {
+//     item.classList.add("item");
+
+//     if (!item.classList.contains("primary")) {
+//         item.classList.add("secondary");
+//     }
+//     console.log(item.className);
+// });
